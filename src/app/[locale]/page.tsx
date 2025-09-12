6c4f124827562205cb3c8065ec535c41abe2fc95
StanislavDevel <unfeel00@gmail.com>
2025-09-12 17:29:22 +0200

    feat:  add MainPage with styles

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,5 @@
-import { useTranslations } from 'next-intl';
+import MainPage from '@/components/MainPage/MainPage';
 
 export default function Home() {
-  const t = useTranslations('HomePage');
-  return <h1>{t('title')}</h1>;
+  return <MainPage />;
 }
---
+++ src/components/MainPage/AuthBlock.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import Link from 'next/link';
+import { useTranslations } from 'next-intl';
+import { useAuth } from '@/providers/AuthProvider/AuthContext';
+
+export default function AuthBlock() {
+  const t = useTranslations();
+  const { user, loading } = useAuth();
+
+  if (loading) return <p>Loading...</p>;
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="form text-center">
+        <h1 className="text-3xl font-bold text-gray-800 mb-6">
+          {!user
+            ? t('MainPage.title')
+            : `${t('MainPage.titleLogin')} ${user.email ?? ''}`}
+        </h1>
+
+        {!user && (
+          <div className="flex flex-col gap-4 w-full">
+            <p className="text-gray-600 mb-8">{t('MainPage.subtitle')}</p>
+            <Link href="/auth/signin" className="btn-action">
+              {t('Auth.login.submit')}
+            </Link>
+            <Link href="/auth/signup" className="btn-action">
+              {t('Auth.register.submit')}
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
