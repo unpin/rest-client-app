7a239982c3f96a35b9a6ec83ab4b20a64ea79ce0
StanislavDevel <unfeel00@gmail.com>
2025-09-12 16:46:42 +0200

    feat: add footer

--- src/providers/AppProviders.tsx
+++ src/providers/AppProviders.tsx
@@ -2,6 +2,7 @@ import { NextIntlClientProvider } from 'next-intl';
 import { ThemeProvider } from './theme/ThemeProvider';
 import { AuthProvider } from '@/providers/AuthProvider/AuthProvider';
 import StickyHeader from '@/components/Header/StickyHeader';
+import Footer from '@/components/Footer/Footer';
 
 export default function AppProviders({ children }: React.PropsWithChildren) {
   return (
@@ -9,6 +10,7 @@ export default function AppProviders({ children }: React.PropsWithChildren) {
       <AuthProvider>
         <StickyHeader />
         <ThemeProvider>{children}</ThemeProvider>
+        <Footer />
       </AuthProvider>
     </NextIntlClientProvider>
   );
