2ebb5f374be3dac92b8f1b57f99a8552f647eecc
German Bilalov <pinncho@gmail.com>
2025-09-20 00:51:02 +0200

    fix(layout): add bottom padding to body to prevent footer overlap

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en">
-      <body>
+      <body className="pb-[80px]">
         <AppProviders>
           <StickyHeader />
           {children}
